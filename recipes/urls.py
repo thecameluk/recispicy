3c7e7ac46513e4b6acff00bbd611aa68c8dc7718
The Camel Dev <dev@thecamel.uk>
2024-09-12 12:43:17 +0100

    Refactoring, Add Models, DB

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from  recipes import views
 
-
+app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/<int:id>/', views.recipes, name='recipes'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
 
-def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
 def contact(request):
     return HttpResponse('Hello Contact')
 
